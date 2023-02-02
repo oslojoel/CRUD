3c511425514ee65bfd5b80fb056949ed509c3907
oslojoel <108557273+oslojoel@users.noreply.github.com>
2023-02-01 20:58:35 -0500

    Add files via upload

---
+++ prueba/conectar.php
@@ -0,0 +1,26 @@
+<?php
+// configuracion para acceder a la BD 
+function conn (){
+$hostname = "localhost";
+$usuariodb = "root";
+$passworddb = "";
+$dbname = "prueba";
+// Cadena de conexion con el servidor
+$conectar = mysqli_connect($hostname,$usuariodb,$passworddb,$dbname);
+return $conectar;
+
+
+
+
+}
+?>
+
+<?php
+try {
+  ## Creamos la variable $dbh que es la conexión completa a la base de datos, pasándole
+  # los parámetros de conexión del host, la base de datos, el usuario y la contraseña
+    $dbh = new PDO("mysql:host=localhost;dbname=prueba", "root", "");
+} catch (PDOException $e){
+    $dbh = $e->getMessage();
+}
+?>
